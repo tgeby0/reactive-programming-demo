5b871de42683949842db46826c23419e45405426
jackson <jackson.kubin@gmail.com>
2024-10-11 19:42:04 -0400

    add express js

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, world")
+import express from 'express';
+import { createServer } from 'node:http';
+
+const app = express();
+const server = createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000/');
+});
