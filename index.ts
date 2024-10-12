e13118334f6d0c98bb1866ea0ed7ec5d831754e5
jackson <jackson.kubin@gmail.com>
2024-10-11 22:21:48 -0400

    add basic socket and mess w modules

--- index.ts
+++ index.ts
@@ -1,16 +1,29 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { join } from 'node:path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static("dist"));
 
 app.get('/', (req, res) => {
-  res.sendFile(join(process.cwd(), 'index.html'));
+    res.sendFile(join(process.cwd(), 'index.html'));
 });
+
+app.use(express.static(join(__dirname, 'public')));
 
 server.listen(3000, () => {
     console.log('Server is running on http://localhost:3000/');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
